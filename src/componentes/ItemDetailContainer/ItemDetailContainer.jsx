b32b3b911d826ea006067f375d60f7e6eea2de23
HugoVielma <hugovielma16@gmail.com>
2022-10-04 18:07:22 -0400

    sigo trabajando

--- src/componentes/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/componentes/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,41 +1,55 @@
 
+import { getDoc } from "@firebase/firestore";
 import React, {useState, useEffect}  from "react";
 import { useParams } from "react-router";
-import producto from "../datos/productos";
+
 import ItemDetail from "../ItemDetail/ItemDetail"
 // import { getFirestore, doc, getDoc } from "@firebase/firestore";
+
+
+
+
+
+// const viewProducto =(id)=>{
+//     return new Promise((resolucion)=>{
+//         setTimeout(() => { 
+//            const productoAl = producto.find((producto)=>{
+//                return parseInt(id) ===  producto.id;
+//            })
+//            resolucion(productoAl)
+//         }, 1000);
+//     });
+// }
+
  
 
-
-
-const viewProducto =(id)=>{
-    return new Promise((resolucion)=>{
-        setTimeout(() => { 
-           const productoAl = producto.find((producto)=>{
-               return parseInt(id) ===  producto.id;
-           })
-           resolucion(productoAl)
-        }, 1000);
-    });
-}
-
+ const ItemDetailContainer =() =>{
  
+    const [productoDetail, setProductoDetail] =  useState({});
+    const {productoId} = useParams();
+    console.log(productoId)
 
-const ItemDetailContainer =() =>{
- 
-    const [producto, setProducto] = useState([]);
-const {itemid} = useParams(); 
-    useEffect(() =>{
-        viewProducto(itemid).then(respuestaPro =>{
-            setProducto(respuestaPro);
-        })
-    }, [itemid]);
-  
-    
+    useEffect(()=>{
+        const viewProducto = async()=>{
+         try {
+          const docRef = doc(db, "producto", productoId);
+          const docSnap = await getDoc(docRef)
+          if (docSnap.exists()){
+             console.log(docSnap.data())
+             setProductoDetail({id: docSnap.id,...docSnap.data()});
+          }else{
+              console.log("no se encuentra")
+          }
+           }
+    }catch{console.error();}    
+    }
+    viewProducto(); 
+ },[productoId]);
+console.log(`productoDetail`,productoDetail);
 return(
     <>
 
-    <ItemDetail producto={producto}/>
+    <ItemDetail producto={productoDetail}/>
   
     </>
 );
